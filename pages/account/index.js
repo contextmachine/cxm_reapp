3ba82e3c1682026c5e9a8488162db17e0c3728cf
Ilya Kuzmin <68997547+kuzmin-i@users.noreply.github.com>
2022-07-29 13:33:32 +0300

    upd

---
+++ pages/account/index.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Router from "next/router";
+import { Skeleton } from "antd";
+
+const Wrapper = styled.div`
+  padding: 30px 15px;
+
+  display: flex;
+  flex-direction: column;
+
+  && > * + * {
+    margin-top: 24px;
+  }
+`;
+
+const HeadTitle = styled.div`
+  font-size: 20px;
+  font-weight: 900;
+`;
+
+const ProjectList = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  && > * + * {
+    margin-top: 16px;
+  }
+`;
+
+const Project = styled.div`
+  width: 100%;
+  padding-bottom: 60%;
+  overflow: hidden;
+  box-shadow: 0 4px 20px -5px rgb(0 0 0 / 40%);
+  border-radius: 10px;
+  position: relative;
+
+  ${({ skeleton }) =>
+    skeleton === true
+      ? `
+    & {
+        height: 180px !important;
+        padding-bottom: 0;
+        box-shadow: 0 4px 20px -5px rgb(0 0 0 / 10%);
+    }
+  `
+      : ``}
+`;
+
+Project.Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+`;
+
+Project.Preview = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(155deg, #1a56f554, #ec6baca6);
+`;
+
+Project.Header = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 15px;
+`;
+
+Project.Title = styled.div`
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+const Account = () => {
+  const handleProjectRedirect = () => {
+    Router.push("/telegram");
+  };
+
+  const [loadingProjects, setLoadingProjects] = useState(true);
+
+  useEffect(() => {
+    const loadProjects = setTimeout(() => {
+      setLoadingProjects(false);
+    }, 900);
+
+    return () => {
+      clearTimeout(loadProjects);
+    };
+  }, []);
+
+  return (
+    <>
+      <Wrapper>
+        <HeadTitle>Проекты</HeadTitle>
+
+        <ProjectList>
+          {loadingProjects ? (
+            <>
+              {Array(4)
+                .fill(1)
+                .map((_, i) => (
+                  <Project skeleton key={`project:${i}`}>
+                    <Project.Wrapper>
+                      <Skeleton.Input
+                        style={{ width: "100&", height: "180px" }}
+                        active
+                      />
+                    </Project.Wrapper>
+                  </Project>
+                ))}
+            </>
+          ) : (
+            <Project onClick={() => handleProjectRedirect()}>
+              <Project.Wrapper>
+                <Project.Preview></Project.Preview>
+                <Project.Header>
+                  <Project.Title>Сцена #1</Project.Title>
+                </Project.Header>
+              </Project.Wrapper>
+            </Project>
+          )}
+        </ProjectList>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Account;
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,5 +1,47 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Script from "next/script";
+import styled from "styled-components";
+import Router from "next/router";
+import { Skeleton, Spin, Row } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const HeadTitle = styled.div`
+  font-size: 30px;
+
+  && span {
+    font-weight: 900;
+  }
+`;
+
+const Wrapper = styled.div`
+  padding: 15px;
+  padding-top: 80px;
+`;
+
+const antIcon = (
+  <LoadingOutlined
+    style={{
+      fontSize: 64,
+      color: "black",
+    }}
+    spin
+  />
+);
+
+const LoadWrapper = styled(Row)`
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  transition: all 0.8s ease-in-out;
+
+  &&[data-status="hidden"] {
+    opacity: 0;
+  }
+
+  &&[data-status="default"] {
+    opacity: 1;
+  }
+`;
 
 const LogAuth = () => {
   const [tgLoaded, setTgLoaded] = useState(false);
@@ -9,6 +51,10 @@ const LogAuth = () => {
   const [last_name, setLast_name] = useState(null);
   const [photo_url, setPhoto_url] = useState(null);
 
+  const [loadingMeta, setLoadingMeta] = useState(true);
+
+  const useFishMeta = true;
+
   useEffect(() => {
     if (tgLoaded) {
       const webapp = window.Telegram.WebApp;
@@ -16,22 +62,40 @@ const LogAuth = () => {
       const initDataUnsafe = webapp.initDataUnsafe;
 
       const { user = {} } = initDataUnsafe;
-      const {
-        id,
-        is_bot,
-        first_name,
-        last_name,
-        username,
-        language_code,
-        photo_url,
-      } = user;
+      const { id, first_name, last_name } = user;
 
-      setUser_id(id);
-      setFirst_name(first_name);
-      setLast_name(last_name);
-      setPhoto_url(photo_url);
+      webapp.expand();
+
+      if (!useFishMeta) {
+        setUser_id(id);
+        setFirst_name(first_name);
+        setLast_name(last_name);
+      } else {
+        setUser_id("310889849");
+        setFirst_name("Ilya");
+        setLast_name("Kuzmin");
+      }
     }
-  }, [tgLoaded]);
+  }, [tgLoaded, useFishMeta]);
+
+  useEffect(() => {
+    if (tgLoaded) {
+      /* визуальная подгрузка имени */
+      const loadMeta = setTimeout(() => {
+        setLoadingMeta(false);
+      }, 500);
+
+      /* редирект на страницу аккаунта */
+      const accountRedirect = setTimeout(() => {
+        Router.push("/account");
+      }, 2000);
+
+      return () => {
+        clearTimeout(loadMeta);
+        clearTimeout(accountRedirect);
+      };
+    }
+  }, [tgLoaded, user_id]);
 
   return (
     <>
@@ -40,12 +104,30 @@ const LogAuth = () => {
         onLoad={() => setTgLoaded(true)}
       ></Script>
 
-      <div>
-        <div>user id: ${user_id}</div>
-        <div>first_name: ${first_name}</div>
-        <div>last_name: ${last_name}</div>
-        <div>photo_url: ${photo_url}</div>
-      </div>
+      <LoadWrapper
+        data-status={loadingMeta ? "hidden" : "default"}
+        justify="center"
+      >
+        <Spin indicator={antIcon} />
+      </LoadWrapper>
+
+      <Wrapper>
+        <div>
+          <HeadTitle>
+            Вы авторизированы как{" "}
+            {loadingMeta ? (
+              <Skeleton.Input
+                active
+                style={{ height: "40px", width: "200px", borderRadius: "20px" }}
+              />
+            ) : (
+              <span>
+                {first_name} {last_name}
+              </span>
+            )}
+          </HeadTitle>
+        </div>
+      </Wrapper>
     </>
   );
 };
