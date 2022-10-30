5c3baf87bd2884f7d97e8661ead5ddcef939cc00
Ilya Kuzmin <59930313+adamkuzmin@users.noreply.github.com>
2022-10-30 15:51:41 +0300

    upd

--- components/scene/mechanics/hooks/unpack-zip-scene.js
+++ components/scene/mechanics/hooks/unpack-zip-scene.js
@@ -10,35 +10,39 @@ const unpackZipScene = async ({
   const url = `${path}?f=gzip`;
 
   /* Шаг 1: fetch ссылки и превращение в unit8Array */
-  let ss = await fetch(url);
-  const sdata = await ss.arrayBuffer();
-  const byteArray = new Uint8Array(sdata);
-
   try {
-    /* Шаг 2: Распаковать архив с помощью pako */
-    let inflated = JSON.parse(pako.inflate(byteArray, { to: "string" }));
+    let ss = await fetch(url);
+    const sdata = await ss.arrayBuffer();
+    const byteArray = new Uint8Array(sdata);
 
-    if (typeof inflated === "object" && inflated?.geometries) {
-      /* Шаг 2.1: тип сцены — цельная с дочерними вложениями */
-      const loader = new THREE.ObjectLoader();
-      loader.parse(
-        inflated,
-        function (obj) {
-          setDataGeometry(obj);
-          SetFetched(true);
-        },
+    try {
+      /* Шаг 2: Распаковать архив с помощью pako */
+      let inflated = JSON.parse(pako.inflate(byteArray, { to: "string" }));
 
-        function (err) {
-          console.error("An error happened");
-        }
-      );
-    } else {
-      /* Шаг 2.2: тип сцены - массив с элементами, надо разбирать каждую */
-      setDataGeometry(inflated);
+      if (typeof inflated === "object" && inflated?.geometries) {
+        /* Шаг 2.1: тип сцены — цельная с дочерними вложениями */
+        const loader = new THREE.ObjectLoader();
+        loader.parse(
+          inflated,
+          function (obj) {
+            setDataGeometry(obj);
+            SetFetched(true);
+          },
 
-      SetFetched(true);
+          function (err) {
+            console.error("An error happened");
+          }
+        );
+      } else {
+        /* Шаг 2.2: тип сцены - массив с элементами, надо разбирать каждую */
+        setDataGeometry(inflated);
+
+        SetFetched(true);
+      }
+    } catch (error) {
+      onFailing();
     }
-  } catch (error) {
+  } catch (err) {
     onFailing();
   }
 };
